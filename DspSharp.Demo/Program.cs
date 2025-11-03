0212e14de4e227be89739ebd4bf33ba8591bb493
Kevin Thompson <antiduh@gmail.com>
2025-11-02 22:07:07 -0500

    Quick demo program for development.

---
+++ DspSharp.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using DspSharp.Buffers;
+using DspSharp.FFTW;
+using DspSharp.FFTW.Float64;
+
+namespace DspSharp.Demo
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            int length = 512;
+            Complex64Array input = new Complex64Array( length, 128 );
+            Complex64Array output = new Complex64Array( length, 128 );
+
+            var plan = Fftw64Builder.Create1( length, input, output, Direction.Forward, Options.Patient );
+
+            input.Clear();
+
+        }
+    }
+}
--- DspSharp.FFTW/Float64/Fftw64Builder.cs
+++ DspSharp.FFTW/Float64/Fftw64Builder.cs
@@ -5,6 +5,7 @@ namespace DspSharp.FFTW.Float64
 {
     public static unsafe class Fftw64Builder
     {
+        // Input and output might be filled with garbage
         public static Fftw64Plan Create1( int n, Complex64Array input, Complex64Array output, Direction dir, Options opts )
         {
             lock( Fftw64ApiLock.Lock )
