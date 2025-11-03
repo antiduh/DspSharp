e21d7e72f243dc84bb083ab7ad3cfa8ed5a19cfa
Kevin Thompson <antiduh@gmail.com>
2025-11-03 09:29:59 -0500

    Fixed the dllimports using the wrong filename.

--- DspSharp.FFTW/Float64/NativeMethods64.cs
+++ DspSharp.FFTW/Float64/NativeMethods64.cs
@@ -17,16 +17,16 @@ namespace DspSharp.FFTW.Float64
             Options flags
         );
 
-        [DllImport( "fftw", EntryPoint = "fftw_execute", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl )]
+        [DllImport( "fftw3", EntryPoint = "fftw_execute", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl )]
         public static extern void Execute( nint handle );
 
-        [DllImport( "fftw", EntryPoint = "fftw_destroy_plan", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl )]
+        [DllImport( "fftw3", EntryPoint = "fftw_destroy_plan", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl )]
         public static extern void DestroyPlan( nint handle );
 
-        [DllImport( "fftw", EntryPoint = "fftw_import_wisdom_from_filename", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode )]
+        [DllImport( "fftw3", EntryPoint = "fftw_import_wisdom_from_filename", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode )]
         public static extern int ImportWisdom( string filename );
 
-        [DllImport( "fftw", EntryPoint = "fftw_export_wisdom_to_filename", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode )]
+        [DllImport( "fftw3", EntryPoint = "fftw_export_wisdom_to_filename", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode )]
         public static extern int ExportWisdom( string filename );
     }
 }
