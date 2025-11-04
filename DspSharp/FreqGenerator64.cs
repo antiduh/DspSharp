f4028f7af9d4b3d2f43c45924df97159f3673cec
Kevin Thompson <antiduh@gmail.com>
2025-11-03 22:05:29 -0500

    Wrote a simple frequency generator.

---
+++ DspSharp/FreqGenerator64.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace DspSharp
+{
+    public class FreqGenerator64
+    {
+        private Complex phasor;
+
+        private Complex angle;
+
+        public FreqGenerator64( int sampleRate, int frequency )
+        {
+            this.angle = new Complex( 1.0, 0.0 );
+
+            double angularStep = 2.0 * Math.PI * frequency / sampleRate;
+
+            this.phasor = Complex.FromPolarCoordinates( 1.0, angularStep );
+        }
+
+        public void Process( Span<Complex> buffer )
+        {
+            for( int i = 0; i < buffer.Length; i++ )
+            {
+                buffer[i] = this.angle;
+                this.angle *= this.phasor;
+            }
+        }
+    }
+}
