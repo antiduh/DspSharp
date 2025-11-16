f0f419d85fffa95dd1ea553bcb3bc3adef9fc3cb
Kevin Thompson <antiduh@gmail.com>
2025-11-15 22:10:04 -0500

    Implemented 64-bit freq shift
    Working on implementing complex64 SIMD math.

---
+++ DspSharp/FreqShift/FastFreqShift64.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using DspSharp.Buffers;
+using DspSharp.Simd;
+
+namespace DspSharp.FreqShift
+{
+    public class FastFreqShift64
+    {
+        private Complex64Array? currBuffer;
+
+        private int position;
+
+        private Complex64Array? newBuffer;
+
+        private PhasorBuilder builder;
+
+        public FastFreqShift64( int sampleRate, int blockSize )
+            : this( sampleRate, blockSize, blockSize * 10, 0.000_000_000_1, 128 )
+        {
+        }
+
+        public FastFreqShift64( int sampleRate, int blockSize, int maxSamples, double epsilon, int memAlignment )
+        {
+            this.builder = new PhasorBuilder( sampleRate, blockSize, maxSamples, epsilon, memAlignment );
+        }
+
+        public void PrepareNewSettings( int freq )
+        {
+            this.newBuffer = this.builder.Build64( freq );
+        }
+
+        public void ApplyNewSettings()
+        {
+            if( this.newBuffer == null )
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Prepare
+            Complex64Array? oldBuffer = this.currBuffer;
+
+            // Swap
+            this.currBuffer = newBuffer;
+            this.position = 0;
+
+            // Clean up.
+            this.newBuffer = null;
+            oldBuffer?.Dispose();
+        }
+
+        public void SetFrequency( int freq )
+        {
+            PrepareNewSettings( freq );
+            ApplyNewSettings();
+        }
+
+
+        public void Process( Span<Complex> output )
+        {
+            Span<Complex> source;
+
+            int outputRemaining = output.Length;
+
+            source = GetChunk( outputRemaining );
+            Complex64Simd.ComplexMultiply( output, output, source );
+
+            outputRemaining -= source.Length;
+
+            if( outputRemaining > 0 )
+            {
+                int outputPos = source.Length;
+
+                source = GetChunk( outputRemaining );
+
+                output = output.Slice( outputPos );
+
+                Complex64Simd.ComplexMultiply( output, output, source );
+            }
+        }
+
+        private Span<Complex> GetChunk( int maxLength )
+        {
+            Span<Complex> source = this.currBuffer.AsSpan();
+
+            // Figure out how much we can provide.
+            int available = source.Length - this.position;
+            int chunkSize = Math.Min( maxLength, available );
+
+            // Slice out the next source chunk.
+            Span<Complex> chunk = source.Slice( this.position, chunkSize );
+
+            // Update accounting.
+            this.position += chunkSize;
+
+            if( this.position > source.Length - 1 )
+            {
+                this.position = 0;
+            }
+
+            return chunk;
+        }
+    }
+}
---
+++ DspSharp/Simd/Complex64Simd.cs
@@ -0,0 +1,134 @@
+﻿// Derived from https://stackoverflow.com/questions/53677757/simd-vectors-complex-numbers
+
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace DspSharp.Simd
+{
+    public static class Complex64Simd
+    {
+        public static void ComplexMultiply( Span<Complex> result, ReadOnlySpan<Complex> left, ReadOnlySpan<Complex> right )
+        {
+            if( Avx.IsSupported )
+            {
+                ComplexMultiply_128( result, left, right );
+            }
+            else if( Avx2.IsSupported )
+            {
+                ComplexMultiply_256( result, left, right );
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public static void ComplexMultiply_128( Span<Complex> result, ReadOnlySpan<Complex> left, ReadOnlySpan<Complex> right )
+        {
+            Span<Vector128<double>> vectorRes = MemoryMarshal.Cast<Complex, Vector128<double>>( result );
+            ReadOnlySpan<Vector128<double>> vectorLeft = MemoryMarshal.Cast<Complex, Vector128<double>>( left );
+            ReadOnlySpan<Vector128<double>> vectorRight = MemoryMarshal.Cast<Complex, Vector128<double>>( right );
+
+            for( int i = 0; i < vectorRes.Length; i++ )
+            {
+                Vector128<double> l = vectorLeft[i];
+                Vector128<double> r = vectorRight[i];
+
+                vectorRes[i] = Avx.HorizontalAdd(
+                    Avx.Multiply(
+                        Avx.Multiply( l, r ),
+                        Vector128.Create( 1.0, -1.0 )
+                    ),
+
+                    Avx.Multiply(
+                        l,
+                        Avx.Permute( r, 0b01 )
+                    )
+                );
+            }
+        }
+
+
+        public static void ComplexMultiply_256( Span<Complex> result, ReadOnlySpan<Complex> left, ReadOnlySpan<Complex> right )
+        {
+            Span<Vector256<double>> vectorRes = MemoryMarshal.Cast<Complex, Vector256<double>>( result );
+            ReadOnlySpan<Vector256<double>> vectorLeft = MemoryMarshal.Cast<Complex, Vector256<double>>( left );
+            ReadOnlySpan<Vector256<double>> vectorRight = MemoryMarshal.Cast<Complex, Vector256<double>>( right );
+            
+            for( int i = 0; i < vectorRes.Length; i++ )
+            {
+                Vector256<double> l = vectorLeft[i];
+                Vector256<double> r = vectorRight[i];
+
+                vectorRes[i] = Avx.HorizontalAdd(
+                    Avx.Multiply(
+                        Avx.Multiply( l, r ),
+                        Vector256.Create( 1.0, -1.0, 1.0, -1.0 ) 
+                    ),
+
+                    Avx.Multiply(
+                        l,
+                        Avx.Permute( r, 0b0101 )
+                    ) 
+                );
+            }
+
+            for( int i = 2 * vectorRes.Length; i < result.Length; i++ )
+            {
+                result[i] = left[i] * right[i];
+            }
+        }
+
+        public static void ComplexMultiply( Span<Complex> result, ReadOnlySpan<Complex> left, Complex single )
+        {
+            Span<Vector256<double>> vectorRes = MemoryMarshal.Cast<Complex, Vector256<double>>( result );
+            ReadOnlySpan<Vector256<double>> vectorLeft = MemoryMarshal.Cast<Complex, Vector256<double>>( left );
+
+            Vector256<double> scalerVect = Vector256.Create( single.Real, single.Imaginary, single.Real, single.Imaginary );
+
+            for( int i = 0; i < vectorRes.Length; i++ )
+            {
+                Vector256<double> l = vectorLeft[i];
+
+
+                vectorRes[i] = Avx.HorizontalAdd(
+                    Avx.Multiply(
+                        Avx.Multiply( l, scalerVect ),
+                        Vector256.Create( 1.0, -1.0, 1.0, -1.0 )
+                    ),
+
+                    Avx.Multiply(
+                        l,
+                        Avx.Permute( scalerVect, 0b0101 )
+                    )
+                );
+            }
+
+            for( int i = 2 * vectorRes.Length; i < result.Length; i++ )
+            {
+                result[i] = left[i] * single;
+            }
+        }
+
+
+        public static void Add( Span<Complex> result, ReadOnlySpan<Complex> left, ReadOnlySpan<Complex> right )
+        {
+            Span<Vector256<double>> vectorRes = MemoryMarshal.Cast<Complex, Vector256<double>>( result );
+            ReadOnlySpan<Vector256<double>> vectorLeft = MemoryMarshal.Cast<Complex, Vector256<double>>( left );
+            ReadOnlySpan<Vector256<double>> vectorRight = MemoryMarshal.Cast<Complex, Vector256<double>>( right );
+
+            for( int i = 0; i < vectorRes.Length; i++ )
+            {
+                vectorRes[i] = Avx.Add( vectorLeft[i], vectorRight[i] );
+            }
+
+            for( int i = 2 * vectorRes.Length; i < result.Length; i++ )
+            {
+                result[i] = left[i] + right[i];
+            }
+        }
+    }
+}
